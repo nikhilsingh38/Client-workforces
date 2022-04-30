97b6cbaef26bb612e3166efa06f882cc943e7d1c
nikhilsingh38 <93506721+nikhilsingh38@users.noreply.github.com>
2022-04-30 23:06:01 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 
 import './App.css';
 import { ToastContainer } from "react-toastify";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <ToastContainer/>
-        <h2>Hello React</h2>
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
-  );
+    </BrowserRouter>
+  )
 }
 
 export default App;
