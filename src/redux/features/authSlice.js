30e1049e698d5c0bec8e9378c123e64849868508
nikhilsingh38 <93506721+nikhilsingh38@users.noreply.github.com>
2022-05-01 00:48:15 +0530

    registration completed

--- src/redux/features/authSlice.js
+++ src/redux/features/authSlice.js
@@ -13,28 +13,53 @@ export const login = createAsyncThunk("auth/login", async ({formValue, navigate,
     }
 })
 
-const authSlice = createSlice({
-    name: "auth",
-    initialState: {
-        user: null,
-        error: "",
-        loading: false
-    },
-    extraReducers: {
-        [login.pending]: (state, action) => {
-            state.loading = true
-        },
-        [login.fulfilled]: (state, action) => {
-            state.loading = false;
-            localStorage.setItem("profile", JSON.stringify({ ...action.payload }));
-            state.user = action.payload
-        },
-        [login.rejected]: (state, action) => {
-            state.loading = false;
-            state.error = action.payload.message;
-                
-        }
+export const register = createAsyncThunk(
+  "auth/register",
+  async ({ formValue, navigate, toast }, { rejectWithValue }) => {
+    try {
+      const response = await api.signUp(formValue);
+      toast.success("Registered Successfully");
+      navigate("/");
+      return response.data;
+    } catch (err) {
+      return rejectWithValue(err.response.data);
     }
+  }
+);
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: null,
+    error: "",
+    loading: false,
+  },
+  extraReducers: {
+    [login.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [login.fulfilled]: (state, action) => {
+      state.loading = false;
+      localStorage.setItem("profile", JSON.stringify({ ...action.payload }));
+      state.user = action.payload;
+    },
+    [login.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload.message;
+    },
+    [register.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [register.fulfilled]: (state, action) => {
+      state.loading = false;
+      localStorage.setItem("profile", JSON.stringify({ ...action.payload }));
+      state.user = action.payload;
+    },
+    [register.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload.message;
+    }
+  },
 });
 
 export default authSlice.reducer;
