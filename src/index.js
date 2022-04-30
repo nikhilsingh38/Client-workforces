ab12b868462ea1b2ffd43b9bb294a4b01fd62975
nikhilsingh38 <93506721+nikhilsingh38@users.noreply.github.com>
2022-05-01 00:20:29 +0530

    login completed

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,7 +9,10 @@ import {
   MDBIcon,
   MDBSpinner,
 } from "mdb-react-ui-kit";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { googleSignIn, login } from "../redux/features/authSlice";
+import { toast } from "react-toastify";
 
 const initialState = {
   email: "",
@@ -18,9 +21,25 @@ const initialState = {
 
 const Login = () => {
   const [formValue, setFormValue] = useState(initialState);
+  const { loading, error } = useSelector((state) => ({ ...state.auth }));
   const { email, password } = formValue;
-  const handleSubmit = () => {};
-  const onInputChange = () => {};
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+   useEffect(() => {
+     error && toast.error(error);
+   }, [error]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email && password) {
+      dispatch(login({ formValue, navigate, toast }));
+    }
+  };
+  const onInputChange = (e) => {
+    let { name, value } = e.target;
+    setFormValue({ ...formValue, [name]: value });
+  };
 
   return (
     <div
@@ -63,15 +82,24 @@ const Login = () => {
             </div>
             <div className="col-12">
               <MDBBtn style={{ width: "100%" }} className="mt-2">
+                {loading && (
+                  <MDBSpinner
+                    size="sm"
+                    role="status"
+                    tag="span"
+                    className="me-2"
+                  />
+                )}
                 Login
               </MDBBtn>
             </div>
           </MDBValidation>
-              </MDBCardBody>
-              <MDBCardFooter>
-                  <Link to="/register">
-                  <p>Don't have an account? Sign up</p></Link>
-              </MDBCardFooter>
+        </MDBCardBody>
+        <MDBCardFooter>
+          <Link to="/register">
+            <p>Don't have an account? Sign up</p>
+          </Link>
+        </MDBCardFooter>
       </MDBCard>
     </div>
   );
