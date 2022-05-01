9fd47a4df4c36cbd24763356ca87056f027f639e
nikhilsingh38 <93506721+nikhilsingh38@users.noreply.github.com>
2022-05-01 23:40:07 +0530

    make it better

--- src/redux/api.js
+++ src/redux/api.js
@@ -4,8 +4,9 @@ const devEnv = process.env.NODE_ENV !== "production";
 
 const { REACT_APP_DEV_API, REACT_APP_PROD_API } = process.env;
 
-
-const API = axios.create({ baseURL: `${devEnv ? REACT_APP_DEV_API : REACT_APP_PROD_API}` });
+const API = axios.create({
+  baseURL: `${devEnv ? REACT_APP_DEV_API : REACT_APP_PROD_API}`,
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
